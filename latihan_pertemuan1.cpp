c60f6bdbc645059931c725e5e9df14a467f490e7
rizqafaiza <rizqafaizaraihanah@gmail.com>
2026-09-08 20:43:51 +0700

    Update latihan_pertemuan1.cpp

--- latihan_pertemuan1.cpp
+++ latihan_pertemuan1.cpp
@@ -2,11 +2,13 @@
 
 int main(){
     int nilai[3][3][4];
+    int isi=2;
 
     for(int i=0; i<3; i++){
         for (int j=0; j<3; j++){
             for (int k=0; k<4; k++){
-                nilai[i][j][k] = (i+j+k+1)*2;
+                nilai[i][j][k] = isi;
+                isi+=2;
                 std::cout << nilai[i][j][k] << " ";
             }
             std::cout << std::endl;
